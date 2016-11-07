a80705b89af9bed33023da68ffd6d797f6b192c3
Brendan Dillon <br.lyle.dillon@gmail.com>
2016-11-07 08:48:38 -0700

    Add log out functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get '/account', to: 'account#show'
 
   resources :users, only: [:new, :create]
-  resources :sessions, only: [:create]
+  resources :sessions, only: [:create, :destroy]
 
   namespace :admin do
     resources :teams, only: [:new, :create, :show]
