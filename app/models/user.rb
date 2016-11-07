b007a583e4ef813d05000ff2b0c6ece42c619e73
Brendan Dillon <br.lyle.dillon@gmail.com>
2016-11-07 13:00:51 -0700

    Add most recent workout to dashboard

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,13 @@ class User < ApplicationRecord
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def last_workout
+    # once we have dates on workouts, change this from created_at
+    if workouts.empty?
+      return nil
+    end
+    last = workouts.last
+    "#{last.distance} mile #{last.activity.name}"
+  end
 end
