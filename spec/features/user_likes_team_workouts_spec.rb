1f6f8ef9e4f532d14aec6b8ba3a3c5b70d462148
Brendan Dillon <br.lyle.dillon@gmail.com>
2016-11-08 13:13:19 -0700

    Write test for workout liking

---
+++ spec/features/user_likes_team_workouts_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'A user likes team workouts' do
+  it 'shows the like underneath the workout' do
+    team = FactoryGirl.create(:team)
+    user_1 = FactoryGirl.create(:user, team: team)
+    user_2 = FactoryGirl.create(:user, team: team)
+    workout = FactoryGirl.create(:workout, user: user_2)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+
+    visit '/team/workouts'
+
+    within('.workout:first-of-type') do
+      expect(page).not_to have_content('1 like')
+      click_on 'like'
+      expect(page).to have_content('1 like')
+    end
+  end
+end
