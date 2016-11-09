861521fc3113fdd6fa70914659aad3ff73370630
Brendan Dillon <br.lyle.dillon@gmail.com>
2016-11-09 08:49:38 -0700

    Add sad path test to ensure user doesn't like workouts multiple times

--- spec/features/user_likes_team_workouts_spec.rb
+++ spec/features/user_likes_team_workouts_spec.rb
@@ -20,4 +20,27 @@ RSpec.describe 'A user likes team workouts' do
       expect(page).to have_content('1 like')
     end
   end
+
+  it 'will not allow a user to like a workout multiple times' do
+    team = FactoryGirl.create(:team)
+    user_1 = FactoryGirl.create(:user, team: team)
+    user_2 = FactoryGirl.create(:user, team: team)
+    FactoryGirl.create(:workout, user: user_2)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+
+    visit '/team/workouts'
+
+    within('.workout:first-of-type') do
+      click_on 'Like'
+    end
+
+    within('.workout:first-of-type') do
+      click_on 'Like'
+    end
+
+    within('.workout:first-of-type') do
+      expect(page).to have_content('1 like')
+    end
+  end
 end
